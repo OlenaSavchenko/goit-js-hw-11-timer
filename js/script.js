1e7268a58f560d874b642176747dd8b54f70bab6
Olena Savchenko <olsavchenko1@gmail.com>
2020-04-07 21:35:51 +0300

    hw-11

---
+++ js/script.js
@@ -0,0 +1,54 @@
+const refs = {
+  days: document.querySelector('span[data-value="days"]'),
+  hours: document.querySelector('span[data-value="hours"]'),
+  minutes: document.querySelector('span[data-value="mins"]'),
+  seconds: document.querySelector('span[data-value="secs"]'),
+};
+
+function getDeltaTime() {
+  setInterval(() => {
+    const targetDate = new Date('Aug 1, 2020');
+    const currentDate = Date.now();
+    const deltaTime = targetDate - currentDate;
+
+    updateClockface(deltaTime);
+  }, 1000);
+}
+getDeltaTime();
+
+function updateClockface(time) {
+  /* Оставшиеся дни: делим значение UTC на 1000 * 60 * 60 * 24, количество
+   * миллисекунд в одном дне (миллисекунды * секунды * минуты * часы)
+   */
+  const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+
+  /*
+   * Оставшиеся часы: получаем остаток от предыдущего расчета с помощью оператора
+   * остатка % и делим его на количество миллисекунд в одном часе
+   * (1000 * 60 * 60 = миллисекунды * минуты * секунды)
+   */
+  const hours = pad(
+    Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+  );
+
+  /*
+   * Оставшиеся минуты: получаем оставшиеся минуты и делим их на количество
+   * миллисекунд в одной минуте (1000 * 60 = миллисекунды * секунды)
+   */
+  const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+
+  /*
+   * Оставшиеся секунды: получаем оставшиеся секунды и делим их на количество
+   * миллисекунд в одной секунде (1000)
+   */
+  const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
+
+  refs.days.textContent = `${days}`;
+  refs.hours.textContent = `${hours}`;
+  refs.minutes.textContent = `${mins}`;
+  refs.seconds.textContent = `${secs}`;
+}
+
+function pad(value) {
+  return String(value).padStart(2, '0');
+}
