9e96c79efa63c903f58296651c5917711ee046da
Olena Savchenko <olsavchenko1@gmail.com>
2020-04-08 21:57:10 +0300

    for review

--- js/script.js
+++ js/script.js
@@ -5,50 +5,39 @@ const refs = {
   seconds: document.querySelector('span[data-value="secs"]'),
 };
 
-function getDeltaTime() {
-  setInterval(() => {
-    const targetDate = new Date('Aug 1, 2020');
-    const currentDate = Date.now();
-    const deltaTime = targetDate - currentDate;
+class CountdownTimer {
+  constructor(date) {
+    this.targetDate = new Date (date);    
+  }
 
-    updateClockface(deltaTime);
-  }, 1000);
-}
-getDeltaTime();
+  getDeltaTime() {
+    setInterval(() => {
+      const currentDate = Date.now();
+      const deltaTime = this.targetDate - currentDate;
+           this.updateClockface(deltaTime);
+    }, 1000);
+  }
 
-function updateClockface(time) {
-  /* Оставшиеся дни: делим значение UTC на 1000 * 60 * 60 * 24, количество
-   * миллисекунд в одном дне (миллисекунды * секунды * минуты * часы)
-   */
-  const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+  updateClockface(time) {
+    const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+    const hours = this.pad(
+      Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+    );
 
-  /*
-   * Оставшиеся часы: получаем остаток от предыдущего расчета с помощью оператора
-   * остатка % и делим его на количество миллисекунд в одном часе
-   * (1000 * 60 * 60 = миллисекунды * минуты * секунды)
-   */
-  const hours = pad(
-    Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
-  );
+    const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
 
-  /*
-   * Оставшиеся минуты: получаем оставшиеся минуты и делим их на количество
-   * миллисекунд в одной минуте (1000 * 60 = миллисекунды * секунды)
-   */
-  const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+    const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
 
-  /*
-   * Оставшиеся секунды: получаем оставшиеся секунды и делим их на количество
-   * миллисекунд в одной секунде (1000)
-   */
-  const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
-
-  refs.days.textContent = `${days}`;
-  refs.hours.textContent = `${hours}`;
-  refs.minutes.textContent = `${mins}`;
-  refs.seconds.textContent = `${secs}`;
+    refs.days.textContent = `${days}`;
+    refs.hours.textContent = `${hours}`;
+    refs.minutes.textContent = `${mins}`;
+    refs.seconds.textContent = `${secs}`;
+  }
+  pad(value) {
+    return String(value).padStart(2, '0');
+  }
 }
 
-function pad(value) {
-  return String(value).padStart(2, '0');
-}
+const counter = new CountdownTimer('Aug 1, 2020');
+counter.getDeltaTime();
+
